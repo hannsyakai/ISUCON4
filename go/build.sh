3caa421e1ff651a5fb29275c637e07cb936f4dc0
upamune <jajkeqos@gmail.com>
2016-10-15 16:29:41 +0900

    pprofを追加

--- go/build.sh
+++ go/build.sh
@@ -4,4 +4,5 @@ go get github.com/go-martini/martini
 go get github.com/go-redis/redis
 go get github.com/martini-contrib/render
 go get github.com/kr/pretty
+go get github.com/pkg/profile
 go build -o golang-webapp .
